10b73e8396f02b72839da7b8535a7efb0b19ee96
Alexander Ignatov <ani1802@abv.bg>
2021-02-21 22:33:46 +0200

    Fix formatting and dirs

---
+++ week1/5.cpp
@@ -0,0 +1,34 @@
+struct Point
+{
+	int x;
+	int y;
+};
+
+struct Rectangle
+{
+	Point origin;
+	int width;
+	int height;
+};
+
+bool pointInRectangle(Point p, Rectangle r)
+{
+	return r.origin.x <= p.x <= r.origin.x + r.width 
+		&& r.origin.y <= p.y <= r.origin.y + r.height;
+}
+
+void translateRectangle(Rectangle& r, int deltaX, int deltaY)
+{
+	r.origin.x += deltaX;
+	r.origin.y += deltaY;
+}
+
+Point* edgesOfRectangle(Rectangle r)
+{
+	Point topLeft = r.origin;
+	Point topRight = { topLeft.x + r.width, topLeft.y };
+	Point bottomLeft = { topLeft.x, topLeft.y + r.height };
+	Point bottomRight = { topLeft.x + r.width, topLeft.y + r.height };
+	Point result[4] = { topLeft, topRight, bottomLeft, bottomRight };
+	return result;
+}
---
+++ week1/6.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+struct RouteCheckpoint
+{
+    RouteCheckpoint *next;
+    char name[1024];
+};
+
+void printRoute(const RouteCheckpoint *start)
+{
+    if (start == nullptr)
+        return; // just in case
+
+    const RouteCheckpoint *currentPoint = start;
+    std::cout << start->name;
+    while (currentPoint->next != nullptr)
+    {
+        currentPoint = currentPoint->next;
+        std::cout << "->" << currentPoint->name;
+    }
+}
+
+void printRouteRecursively(const RouteCheckpoint *start)
+{
+    if (start == nullptr)
+    {
+        return;
+    }
+
+    std::cout << start->name;
+
+    if (start->next != nullptr)
+    {
+        std::cout << "->";
+    }
+
+    printRouteRecursively(start->next);
+}
