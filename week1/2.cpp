0944edc8274573b1d88d15ec0a8cdc0268b49310
Alexander Ignatov <ani1802@abv.bg>
2021-02-21 20:52:25 +0200

    Add solution to task 2 from week 1

---
+++ week1/2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+int main()
+{
+    int n, m;
+
+    std::cin >> n >> m;
+
+    int **matrix = new int *[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        matrix[i] = new int[m];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            int newNumber;
+            std::cin >> newNumber;
+            matrix[i][j] = newNumber;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << "| ";
+        for (int j = 0; j < m; j++)
+        {
+            std::cout << matrix[i][j] % 10 << " ";
+        }
+        std::cout << "|" << std::endl;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        delete[] matrix[i];
+    }
+
+    delete[] matrix;
+
+    return 0;
+}
